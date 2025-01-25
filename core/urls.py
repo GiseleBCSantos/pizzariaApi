8507a76e84eb2e77c3adfcbaedd80ad89fe5d61f
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2025-01-25 00:34:14 -0300

    :sparkles: Add Post, Get, Delete endpoints

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,13 @@ from .views import *
 
 urlpatterns = [
     path("cliente/", ListCreateClienteApiView.as_view()),
+    path("cliente/<int:pk>", RetrieveUpdateDestroyClienteApiView.as_view()),
     path("pizza/", ListCreatePizzaApiView.as_view()),
+    path("pizza/<int:pk>", RetrieveUpdateDestroyPizzaApiView.as_view()),
     path("tamanho/", ListCreateTamanhoApiView.as_view()),
+    path("tamanho/<int:pk>", RetrieveUpdateDestroyTamanhoApiView.as_view()),
     path("pedido/", ListCreatePedidoApiView.as_view()),
+    path("pedido/<int:pk>", RetrieveUpdateDestroyPedidoApiView.as_view()),
     path("itempedido/", ListCreateItemPedidoApiView.as_view()),
+    path("itempedido/<int:pk>", RetrieveUpdateDestroyItemPedidoApiView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import *
 from .models import *
 
@@ -8,9 +8,19 @@ from .models import *
 class ListCreateClienteApiView(ListCreateAPIView):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    
+
+
+class RetrieveUpdateDestroyClienteApiView(RetrieveUpdateDestroyAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
 
 class ListCreatePizzaApiView(ListCreateAPIView):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class RetrieveUpdateDestroyPizzaApiView(RetrieveUpdateDestroyAPIView):
     queryset = Pizza.objects.all()
     serializer_class = PizzaSerializer
     
@@ -18,13 +28,27 @@ class ListCreatePizzaApiView(ListCreateAPIView):
 class ListCreateTamanhoApiView(ListCreateAPIView):
     queryset = Tamanho.objects.all()
     serializer_class = TamanhoSerializer
+
+
+class RetrieveUpdateDestroyTamanhoApiView(RetrieveUpdateDestroyAPIView):
+    queryset = Tamanho.objects.all()
+    serializer_class = TamanhoSerializer
     
     
 class ListCreatePedidoApiView(ListCreateAPIView):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
-    
+
+
+class RetrieveUpdateDestroyPedidoApiView(RetrieveUpdateDestroyAPIView):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
 
 class ListCreateItemPedidoApiView(ListCreateAPIView):
+    queryset = ItensPedido.objects.all()
+    serializer_class = ItemPedidoSerializer
+
+
+class RetrieveUpdateDestroyItemPedidoApiView(RetrieveUpdateDestroyAPIView):
     queryset = ItensPedido.objects.all()
     serializer_class = ItemPedidoSerializer
